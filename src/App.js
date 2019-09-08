489bb678aab24940dfee2b9afaac355a9414be91
Stiliyana Yordanova <stiliyana@door2door.io>
2019-09-08 23:44:48 +0200

    Add Ohne website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Layout from "./components/Layout"
+import Header from "./components/Header"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Contact from "./components/Contact"
+import Map from "./components/Map"
+import Tabletop from 'tabletop'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedVenueType: null,
+    filteredVenues: [],
+    isFilterOpen: true
+  }
+
+  venues = []
+
+  componentDidMount() {
+    window.addEventListener('scroll', this._handleScroll)
+    this._getVenues()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this._handleScroll)
+  }
+
+  _getVenues = () => {
+    Tabletop.init({
+      key: process.env.REACT_APP_SHEETS_KEY,
+      simpleSheet: true,
+      callback: (data, tabletop) => {
+        const mappedVenues = data.map(venue => ({ ...venue, latitude: Number(venue.latitude), longitude: Number(venue.longitude)}))
+        this.setState({
+          filteredVenues: mappedVenues
+        })
+        this.venues = mappedVenues
+      }
+    })
+  }
+
+  _handleScroll = event => {
+    if (window.scrollY > window.innerHeight) {
+      this.setState({
+        isFilterOpen: false
+      })
+    }
+
+  }
+
+  _handleFilterToggle = () => {
+    this.setState({
+      isFilterOpen: !this.state.isFilterOpen
+    })
+  }
+
+  _handleSelectVenueType = (event) => {
+    this.setState({
+      selectedVenueType: event.target.value,
+      filteredVenues: this.venues.filter(v => v.type === event.target.value)
+    })
+  }
+
+  _handleFilterClear = () => {
+    this.setState({
+      selectedVenueType: null,
+      filteredVenues: this.venues
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Header
+          onCheckboxSelect={this._handleSelectVenueType}
+          selectedVenueType={this.state.selectedVenueType}
+          isFilterOpen={this.state.isFilterOpen}
+          onFilterToggle={this._handleFilterToggle}
+          onFilterClear={this._handleFilterClear}
+        />
+        <Map venues={this.state.filteredVenues} />
+        <About />
+        <Contact />
+        <Footer />
+      </Layout>
+    )
+  }
 }
 
-export default App;
+export default App
