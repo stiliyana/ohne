f7782cd3a680cf62975d7edeb5e92342442b1479
Stiliyana Yordanova <stiliyana.yordanova@gmail.com>
2020-07-18 14:10:52 +0200

    Fix linter offence

--- src/typography/Link/index.js
+++ src/typography/Link/index.js
@@ -4,7 +4,7 @@ import injectSheet from 'react-jss'
 import styles from './styles'
 
 const _Link = ({ children, href, classes }) => (
-  <a className={classes.root} target="_blank" rel="noreferrer" href={href} rel="noopener noreferrer">
+  <a className={classes.root} target="_blank" href={href} rel="noopener noreferrer">
     { children }
   </a>
 )
